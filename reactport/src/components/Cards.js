6ca79c2e6eb76d0b2af11f3650c591c976f2a5a9
ggstoltz <gunnar.b.stoltz@gmail.com>
2021-01-04 23:49:53 -0600

    add images
    adding card images

--- reactport/src/components/Cards.js
+++ reactport/src/components/Cards.js
@@ -9,19 +9,19 @@ function Cards() {
             <div classname='cards__container'>
               <ul className='cards__items' >
                   <Carditem 
-                  src="img-9.jpg"
+                  src="img-1.png"
                   text="this is app 1"
                   label='Application'
                   path='/services'
                   />
                   <Carditem 
-                  src="img-9.jpg"
+                  src="img-2.png"
                   text="this is app 2"
                   label='Application'
                   path='/services'
                   />
                   <Carditem 
-                  src="img-9.jpg"
+                  src="img-3.jpg"
                   text="this is app 3"
                   label='Application'
                   path='/services'
