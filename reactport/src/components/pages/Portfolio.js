0b307a13fb19d4277024b045c2d75576755144f8
ggstoltz <gunnar.b.stoltz@gmail.com>
2021-01-09 17:26:23 -0600

    fix
    Fix

--- reactport/src/components/pages/Portfolio.js
+++ reactport/src/components/pages/Portfolio.js
@@ -4,9 +4,12 @@ import '../../App.css'
 
 export default function Portfolio() {
     return <h1 className='portfolio'>PORTFOLIO </h1>;
-   
-    
+
+
 }
+
+
+
 
 <p>test </p>
 /* window.location.href = 'https://gunnarstoltz.com'; */
